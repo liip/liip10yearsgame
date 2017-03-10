cbbcd8125188773fc97cbe3c7f660c58f89b078f
Christof Leuenberger <christof.leuenberger@liip.ch>
2017-03-10 09:40:27 +0100

    updated names

--- src/states/Game.js
+++ src/states/Game.js
@@ -7,7 +7,7 @@ export default class extends Phaser.State {
   preload () {}
 
   create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
+    const bannerText = 'Liip 10 Years Timeline Game'
     let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
     banner.font = 'Bangers'
     banner.padding.set(10, 16)
