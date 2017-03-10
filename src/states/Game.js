e8edf382667a34f87bbceaed087bf0132ac32c13
syzer3@gmail.com <syzer3@gmail.com>
2017-03-10 11:51:27 +0100

    ;

--- src/states/Game.js
+++ src/states/Game.js
@@ -10,8 +10,8 @@ export default class extends Phaser.State {
 	}
 
 	create() {
-		this.map = this.game.add.tilemap('level1');
-		this.map.addTilesetImage('tiles_spritesheet', 'gameTiles');
+		this.map = this.game.add.tilemap('level1')
+		this.map.addTilesetImage('tiles_spritesheet', 'gameTiles')
 
 		// create layers
 		this.backgroundLayer = this.map.createLayer('backgroundLayer')
@@ -29,8 +29,8 @@ export default class extends Phaser.State {
 		this.game.camera.follow(this.player)
 
 		// score
-		this.scoreLabel = game.add.text(config.gameWidth-70, 30, 'score: 0', { font: '14px Open Sans', fill: '#414141' });
-		this.score = 0;
+		this.scoreLabel = game.add.text(config.gameWidth-70, 30, 'score: 0', { font: '14px Open Sans', fill: '#414141' })
+		this.score = 0
 
 		// init keys
 		// this.cursors = this.game.input.keyboard.createCursorKeys()
@@ -39,54 +39,54 @@ export default class extends Phaser.State {
 			'down': Phaser.KeyCode.DOWN
 		})
 
-		var playerDuckImg = this.game.cache.getImage('playerDuck');
-		this.player.duckedDimensions = {width: playerDuckImg.width, height: playerDuckImg.height};
-		this.player.standDimensions = {width: this.player.width, height: this.player.height};
-		this.player.anchor.setTo(0.5, 1);
+		let playerDuckImg = this.game.cache.getImage('playerDuck')
+		this.player.duckedDimensions = {width: playerDuckImg.width, height: playerDuckImg.height}
+		this.player.standDimensions = {width: this.player.width, height: this.player.height}
+		this.player.anchor.setTo(0.5, 1)
 
 		// make the player move sideways continuously
-		// this.player.body.velocity.x = 200;
+		this.player.body.velocity.x = 200
 	}
 
 	update() {
 		this.game.physics.arcade.collide(this.player, this.blockedLayer, this.playerHit, null, this)
 
 		if (this.keys.space.isDown) {
-			this.playerJump();
+			this.playerJump()
 		} else if (this.keys.down.isDown) {
-			this.playerDuck();
+			this.playerDuck()
 		}
 
 		if (!this.keys.down.isDown && this.player.isDucked) {
 			//change image and update the body size for the physics engine
-			this.player.loadTexture('player');
-			this.player.body.setSize(this.player.standDimensions.width, this.player.standDimensions.height);
-			this.player.isDucked = false;
+			this.player.loadTexture('player')
+			this.player.body.setSize(this.player.standDimensions.width, this.player.standDimensions.height)
+			this.player.isDucked = false
 		}
 
 		//restart the game if reaching the edge
 
 		if (this.player.x >= this.game.world.width) {
-			this.game.time.events.add(1500, this.gameOver, this);
+			this.game.time.events.add(1500, this.gameOver, this)
 		}
 	}
 
 	gameOver() {
-		this.game.state.start('Game');
+		this.game.state.start('Game')
 	}
 
 	playerJump() {
 		if (this.player.body.blocked.down) {
-			this.player.body.velocity.y -= 700;
+			this.player.body.velocity.y -= 700
 		}
 	}
 
 	playerDuck() {
 		//change image and update the body size for the physics engine
-		this.player.loadTexture('playerDuck');
-		this.player.body.setSize(this.player.duckedDimensions.width, this.player.duckedDimensions.height);
+		this.player.loadTexture('playerDuck')
+		this.player.body.setSize(this.player.duckedDimensions.width, this.player.duckedDimensions.height)
 		//we use this to keep track whether it's ducked or not
-		this.player.isDucked = true;
+		this.player.isDucked = true
 	}
 
 	playerHit(player, blockedLayer) {
@@ -94,13 +94,13 @@ export default class extends Phaser.State {
 
 		if (player.body.blocked.right) {
 			//set to dead (this doesn't affect rendering)
-			this.player.alive = false;
+			this.player.alive = false
 			//stop moving to the right
-			this.player.body.velocity.x = 0;
+			this.player.body.velocity.x = 0
 			//change sprite image
-			this.player.loadTexture('playerDead');
+			this.player.loadTexture('playerDead')
 			//go to gameover after a few miliseconds
-			this.game.time.events.add(1500, this.gameOver, this);
+			this.game.time.events.add(1500, this.gameOver, this)
 		}
 	}
 
