c7f928b9e5643fdd01ca526f0f4668429c7ba5ea
Christof Leuenberger <christof.leuenberger@liip.ch>
2017-03-10 10:45:39 +0100

    fixed indentation

--- src/states/Game.js
+++ src/states/Game.js
@@ -3,59 +3,68 @@ import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
 
 export default class extends Phaser.State {
-  init () {}
-  preload () {}
+	init () {}
+	preload () {}
 
-  create () {
-	this.map = this.game.add.tilemap('level1');
-	this.map.addTilesetImage('tiles_spritesheet', 'gameTiles');
+	create () {
+		this.map = this.game.add.tilemap('level1');
+		this.map.addTilesetImage('tiles_spritesheet', 'gameTiles');
 
-	// create layers
-	this.backgroundLayer = this.map.createLayer('backgroundLayer')
-	this.blockedLayer = this.map.createLayer('blockedLayer')
-	this.map.setCollisionBetween(1, 100000, true, 'blockedLayer')
-	this.backgroundLayer.resizeWorld()
+		// create layers
+		this.backgroundLayer = this.map.createLayer('backgroundLayer')
+		this.blockedLayer = this.map.createLayer('blockedLayer')
+		this.map.setCollisionBetween(1, 100000, true, 'blockedLayer')
+		this.backgroundLayer.resizeWorld()
 
-	// setup player
-	this.player = this.game.add.sprite(100, 200, 'player')
-	this.game.physics.arcade.enable(this.player)
-	this.player.body.gravity.y = 1000
-	this.game.camera.follow(this.player)
+		// setup player
+		this.player = this.game.add.sprite(100, 200, 'player')
+		this.game.physics.arcade.enable(this.player)
+		this.player.body.gravity.y = 1000
+		this.game.camera.follow(this.player)
 
-	// init keys
-	// this.cursors = this.game.input.keyboard.createCursorKeys()
-	this.keys = this.game.input.keyboard.addKeys({
-		'space': Phaser.KeyCode.SPACEBAR
-	})
+		// init keys
+		this.cursors = this.game.input.keyboard.createCursorKeys()
+		this.keys = this.game.input.keyboard.addKeys({
+			'space': Phaser.KeyCode.SPACEBAR
+		})
 
-	var playerDuckImg = this.game.cache.getImage('playerDuck');
-	this.player.duckedDimensions = {width: playerDuckImg.width, height: playerDuckImg.height};
-	this.player.standDimensions = {width: this.player.width, height: this.player.height};
-	this.player.anchor.setTo(0.5, 1);
+		var playerDuckImg = this.game.cache.getImage('playerDuck')
+		this.player.duckedDimensions = { width: playerDuckImg.width, height: playerDuckImg.height }
+		this.player.standDimensions = { width: this.player.width, height: this.player.height }
+		this.player.anchor.setTo(0.5, 1);
 
-	// make the player move sideways continuously
-	this.player.body.velocity.x = 120;
-  }
-
-  update () {
-	this.game.physics.arcade.collide(this.player, this.blockedLayer, this.playerHit, null, this)
-
-	// this.player.body.velocity.x = 300; 
-	if (this.keys.space.isDown) {
-		this.playerJump();
+		// make the player move sideways continuously
+		this.player.body.velocity.x = 120;
 	}
-  }
 
-  playerJump () {
-	if (this.player.body.blocked.down) {
-		this.player.body.velocity.y -= 700;
+	update () {
+		this.game.physics.arcade.collide(this.player, this.blockedLayer, this.playerHit, null, this)
+
+		// this.player.body.velocity.x = 300;
+		if (this.keys.space.isDown) {
+			this.playerJump()
+		}
+		else if (this.cursors.down.isDown) {
+			this.playerDuck()
+		}
 	}
-  }
 
-  playerHit (player, blockedLayer) {
-	this.game.physics.arcade.collide(this.player, this.blockedLayer)
-  }
+	playerJump () {
+		if (this.player.body.blocked.down) {
+			this.player.body.velocity.y -= 700;
+		}
+	}
 
-  render () {
-  }
+	playerJump () {
+		if (this.player.body.blocked.down) {
+			this.player.body.velocity.y -= 700;
+		}
+	}
+
+	playerHit (player, blockedLayer) {
+		this.game.physics.arcade.collide(this.player, this.blockedLayer)
+	}
+
+	render () {
+	}
 }
