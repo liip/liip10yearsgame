60b62f640a38fbb3987741f5eba57f6540d8d864
Christof Leuenberger <christof.leuenberger@liip.ch>
2017-03-10 15:08:08 +0100

    cleaned up splash screen

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -6,17 +6,19 @@ export default class extends Phaser.State {
 	init () {}
 
 	preload () {
-		this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
-		this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-
-		this.logo = this.game.add.sprite(300, 50, 'liipLogo')
+		this.logo = this.game.add.sprite(
+			this.game.world.centerX + 20,
+			100,
+			'liipLogo')
 		this.logo.scale.setTo(0.5)
+		this.logo.anchor.setTo(0.5)
 
-		let welcome = this.game.add.text(50, 30, 'Welcome', Object.assign(config.text.lg, { boundsAlignH: "center" }))
+		let welcome = this.game.add.text(
+			0, 100,
+			'0% loaded',
+			Object.assign(config.text.lg, { boundsAlignH: 'center', boundsAlignV: 'middle' }))
+		welcome.setTextBounds(0, 100, config.gameWidth, 100)
 
-		// centerGameObjects([this.loaderBg, this.loaderBar])
-
-		this.load.setPreloadSprite(this.loaderBar)
 	}
 
 	create () {
