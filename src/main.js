85ddfa69c2a2e3ca1179c4ca1bec3a350ff7dc5a
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-03-20 18:51:54 +0100

    fix code style

--- src/main.js
+++ src/main.js
@@ -14,23 +14,22 @@ import config from './config'
 import './extensions'
 
 class Game extends Phaser.Game {
+	constructor() {
+		const docElement = document.documentElement
+		const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
+		const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
 
-  constructor () {
-    const docElement = document.documentElement
-    const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
-    const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
+		super(width, height, Phaser.CANVAS, 'content', null)
 
-    super(width, height, Phaser.CANVAS, 'content', null)
+		this.state.add('Boot', BootState, false)
+		this.state.add('Splash', SplashState, false)
+		this.state.add('Intro', IntroState, false)
+		this.state.add('Game', GameState, false)
+		this.state.add('GameOver', GameOver, false)
+		this.state.add('HighScore', HighScore, false)
 
-    this.state.add('Boot', BootState, false)
-    this.state.add('Splash', SplashState, false)
-    this.state.add('Intro', IntroState, false)
-    this.state.add('Game', GameState, false)
-    this.state.add('GameOver', GameOver, false)
-    this.state.add('HighScore', HighScore, false)
-
-    this.state.start('Boot')
-  }
+		this.state.start('Boot')
+	}
 }
 
 window.game = new Game()
