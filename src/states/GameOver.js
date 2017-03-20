903d043dc7180cededd2a5dd755350b2da4fb351
Christof Leuenberger <christof.leuenberger@liip.ch>
2017-03-20 19:28:25 +0100

    added replay button to gameover state

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -14,8 +14,24 @@ export default class extends Phaser.State {
 			Object.assign(config.text.xl, ({ boundsAlignH: "center" })))
 		gameOver.setTextBounds(0, 30, this.game.width, 30);
 
+		// replay button
+		let replay = this.game.add.button(
+			this.game.width / 2,
+			250,
+			'replay',
+			() => { this.game.state.start('Game') },
+			this,
+			1,
+			0,
+			1)
+		replay.scale.setTo(0.7)
+		centerGameObjects([replay])
+
 		// init input (keyboard or tap on mobile)
 		this.input = new Input({ game: this.game })
+
+		// @todo remove objects and labels
+		// @todo add replay button
 	}
 
 	update () {
