c47137c0c921ea317e9e24ef45714335ed6ddce7
Christof Leuenberger <christof.leuenberger@liip.ch>
2017-03-20 11:08:33 +0100

    moved preloading of main game assets to loading screen

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -16,31 +16,6 @@ export default class extends Phaser.State {
 			},
 			active: this.fontsLoaded
 		})
-
-		this.load.image('loaderBg', './assets/images/loader-bg.png')
-		this.load.image('loaderBar', './assets/images/preloader.gif')
-
-		this.load.image('player', './assets/images/player.png')
-		this.load.image('playerDuck', './assets/images/player_duck.png')
-		this.load.image('playerDead', './assets/images/player_dead.png')
-
-		this.load.image('liipLogo', './assets/images/liip_logo.png')
-		this.load.image('startLogo', './assets/images/start_logo.png')
-		this.load.image('liipLogoSmall', './assets/images/liip_logo_small.png')
-
-		this.load.image('beer', './assets/images/beer.png')
-		this.load.image('award', './assets/images/award.png')
-		this.load.image('coffee', './assets/images/coffee.png')
-
-		this.load.tilemap('liip', 'assets/tilemaps/liip.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.image('gameTiles', 'assets/images/tiles_spritesheet.png')
-
-		this.load.image('replay', 'assets/images/replay.png')
-		this.load.image('start', 'assets/images/start.png')
-
-		this.load.audio('coin', 'assets/audio/coin.wav')
-		this.load.audio('ouch', 'assets/audio/ouch.wav')
-		this.load.audio('jump', 'assets/audio/jump.ogg')
 	}
 
 	create() {
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -6,10 +6,32 @@ export default class extends Phaser.State {
 
 	preload () {
 		let loading = this.game.add.text(
-			0, 40,
+			0, 0,
 			'loading',
 			Object.assign(config.text.lg, { boundsAlignH: 'center', boundsAlignV: 'middle' }))
-		loading.setTextBounds(0, 100, config.gameWidth, 100)
+		loading.setTextBounds(0, 100, this.game.width, 100)
+
+		this.load.image('player', './assets/images/player.png')
+		this.load.image('playerDuck', './assets/images/player_duck.png')
+		this.load.image('playerDead', './assets/images/player_dead.png')
+
+		this.load.image('liipLogo', './assets/images/liip_logo.png')
+		this.load.image('startLogo', './assets/images/start_logo.png')
+		this.load.image('liipLogoSmall', './assets/images/liip_logo_small.png')
+
+		this.load.image('beer', './assets/images/beer.png')
+		this.load.image('award', './assets/images/award.png')
+		this.load.image('coffee', './assets/images/coffee.png')
+
+		this.load.tilemap('liip', 'assets/tilemaps/liip.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.image('gameTiles', 'assets/images/tiles_spritesheet.png')
+
+		this.load.image('replay', 'assets/images/replay.png')
+		this.load.image('start', 'assets/images/start.png')
+
+		this.load.audio('coin', 'assets/audio/coin.wav')
+		this.load.audio('ouch', 'assets/audio/ouch.wav')
+		this.load.audio('jump', 'assets/audio/jump.ogg')
 
 	}
 
