7438b56dcbbdf560d4e6b18ecb9a300b65ecd268
Christof Leuenberger <christof.leuenberger@liip.ch>
2017-03-20 12:20:45 +0100

    use SCALE_ALL mode on non-desktop devices

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -19,8 +19,12 @@ export default class extends Phaser.State {
 	}
 
 	create() {
-		this.scale.pageAlignHorizontally = true
-		this.scale.pageAlignVertically = true
+		this.game.scale.pageAlignHorizontally = true
+		this.game.scale.pageAlignVertically = true
+
+		if (!this.game.device.desktop) {
+			this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+		}
 
 		this.game.physics.startSystem(Phaser.Physics.ARCADE)
 	}
