5fa47f320f0fb1738823ddc690ae46cffaf28028
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-03-21 20:10:48 +0100

    fix assets paths

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -21,18 +21,18 @@ export default class extends Phaser.State {
 		this.load.image('office', './assets/images/office.png')
 		this.load.image('people', './assets/images/people.png')
 
-		this.load.tilemap('liip', 'assets/tilemaps/liip.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.image('gameTiles', 'assets/images/tiles_spritesheet.png')
+		this.load.tilemap('liip', './assets/tilemaps/liip.json', null, Phaser.Tilemap.TILED_JSON)
+		this.load.image('gameTiles', './assets/images/tiles_spritesheet.png')
 
-		this.load.spritesheet('replay', 'assets/images/replay.png', 140, 140)
-		this.load.spritesheet('start', 'assets/images/start.png', 140, 140)
+		this.load.spritesheet('replay', './assets/images/replay.png', 140, 140)
+		this.load.spritesheet('start', './assets/images/start.png', 140, 140)
 
-		this.load.spritesheet('player', 'assets/images/hero-normal.png', 90, 70, 4)
-		this.load.spritesheet('playerDead', 'assets/images/hero-dead.png', 90, 70, 3)
+		this.load.spritesheet('player', './assets/images/hero-normal.png', 90, 70, 4)
+		this.load.spritesheet('playerDead', './assets/images/hero-dead.png', 90, 70, 3)
 
-		this.load.audio('coin', 'assets/audio/coin.wav')
-		this.load.audio('ouch', 'assets/audio/ouch.wav')
-		this.load.audio('jump', 'assets/audio/jump.ogg')
+		this.load.audio('coin', './assets/audio/coin.wav')
+		this.load.audio('ouch', './assets/audio/ouch.wav')
+		this.load.audio('jump', './assets/audio/jump.ogg')
 
 	}
 
