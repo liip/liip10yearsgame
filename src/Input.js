4d9a9529d33b96fa4725bbc6b918226f89b3fd47
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-03-24 18:18:07 +0100

    separate input handler for desktop and mobile

--- src/Input.js
+++ src/Input.js
@@ -4,21 +4,28 @@ import Phaser from 'phaser'
  * Class for handling input interaction with keyboard and touch devices
  */
 export default class {
-	constructor({ game }) {
+	constructor({ game, muteBtn }) {
 		this.jump = false
 		this.game = game
+		this.muteBtn = muteBtn
 
-		// add handling for spacebar
-		let space = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
-		space.onDown.add(() => {
-			this.jump = true
-		})
-
-		// add handling for tapping
-		this.game.input.onDown.add(() => {
-			this.jump = true
-		})
-
+		if(this.game.device.desktop) {
+			// add handling for spacebar
+			let space = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
+			space.onDown.add(() => {
+				this.jump = true
+			})
+		} else {
+			// add handling for tapping
+			this.game.input.onDown.add((e) => {
+				// don't jump when muteBtn is clicked
+				if(this.muteBtn && e.x < this.muteBtn.x + this.muteBtn.width + 10 && e.y > this.muteBtn.y - muteBtn.height) {
+					// muteBtn pressed
+				} else {
+					this.jump = true
+				}
+			})
+		}
 	}
 
 	/**
