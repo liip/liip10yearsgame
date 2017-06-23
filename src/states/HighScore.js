2dbcb90c6d0a39d6ab46d8d23f9283c44a546147
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-06-23 12:03:51 +0200

    highscore styling

--- src/states/HighScore.js
+++ src/states/HighScore.js
@@ -16,30 +16,37 @@ export default class extends Phaser.State {
         const game = this.game
         const { width, height } = this.game
         const { xl, xxl } = config.text
-        const margin = 50
-        game.add.text(margin, 50, 'Top 5 Scores', xxl)
-        const logo = this.game.add.sprite(margin, 100, 'liipLogo')
-        logo.scale.setTo(0.62)
 
-        let restart = game.add.button(width / 2, height / 2 + 100,
-            'start',
-            () => game.state.start('Game'),
-            this,
-            1, 0, 1)
-        restart.scale.setTo(0.7)
-        restart.anchor.set(0.5, 0)
+        const logo = game.add.sprite(width / 2 - 110, 70, 'liipLogo')
+        logo.scale.setTo(0.3)
+        logo.anchor.set(0, 1)
+        const title = game.add.text(width / 2 + 110, 80, 'TOP 5', xxl)
+        title.anchor.set(1, 1)
 
-        const idColumn = game.add.text(margin, 150, 'Player:', makeGreen(xl))
-        const ScoreColumn = game.add.text(margin + 200, 150, 'Score:', makeGreen(xl))
+        const idColumn = game.add.text(width / 2 - 100, 120, 'Player:', makeGreen(xl))
+        idColumn.anchor.set(0.5, 1)
+        const ScoreColumn = game.add.text(width / 2 + 100, 120, 'Score:', makeGreen(xl))
+        ScoreColumn.anchor.set(0.5, 1)
 
         _.map(this.db.loadScore(), (score, winner) => [score, winner])
             .sort((a, b) => b[0] - a[0])
             .splice(0, 5)
             .map(([score, w], i) => {
-                const nr = game.add.text(margin, 200 + i * 40, `${i + 1}.`, makeGreen(xxl))
-                const winnerText = game.add.text(margin + 50, 200 + i * 40, `${w}`, xxl)
-                const scoreText = game.add.text(margin + 200, 200 + i * 40, `${score}`, xxl)
+                const nr = game.add.text(width / 2 - 130, 160 + i * 40, `${i + 1}.`, makeGreen(xl))
+                nr.anchor.set(0.5, 1)
+                const winnerText = game.add.text(width / 2 - 80, 160 + i * 40, `${w}`, xl)
+                winnerText.anchor.set(0.5, 1)
+                const scoreText = game.add.text(width / 2 + 100, 160 + i * 40, `${score}`, xl)
+                scoreText.anchor.set(0.5, 1)
             })
+
+        let restart = game.add.button(width / 2, 410,
+            'start',
+            () => game.state.start('Game'),
+            this,
+            1, 0, 1)
+        restart.scale.setTo(0.6)
+        restart.anchor.set(0.5, 1)
     }
 
     update() {
