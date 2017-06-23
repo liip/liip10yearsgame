19f4c6b870eedb2ba145b925a00b2aa784cf32e1
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-06-23 14:39:40 +0200

    fix highscore if user is not already there

--- src/objects/Db.js
+++ src/objects/Db.js
@@ -39,7 +39,7 @@ export default class extends Phaser.Sprite {
     addPlayerScore(score, winnerName) {
         const highScores = this.loadScore()
         // only update highscore of player if it's higher than already saved value
-        if(score > highScores[winnerName]) {
+        if(!highScores[winnerName] || score > highScores[winnerName]) {
             highScores[winnerName] = score
             this.saveScore(highScores)
         }
